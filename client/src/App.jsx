cc111cb6f74aa48eac423ecce5e31d4db52a948f
Rajesh-dot-coder <rajesh9786951412@gmail.com>
2025-05-29 21:00:54 +0530

    updated the signin page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 import Home from './pages/Home'
+import Signin from './pages/Signin'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Home/>
+      {/* <Home/> */}
+      <Signin/>
     </>
   )
 }
